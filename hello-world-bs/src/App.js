9b4185ddddd0a73e0331a200c99ac207ebc04399
codrelphi <chancerel.codjovi@gmail.com>
2019-11-16 20:47:02 +0100

    write the code using state (of react)

--- hello-world-bs/src/App.js
+++ hello-world-bs/src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 
 class App extends React.Component {
 
+  state = {
+    name: "John Doe"
+  }
+
+  handleName(e){
+    this.setState({name: e.target.value});
+  }
   render() {
     return (<div className="container">
               <div className="row">
                 <div className="col"></div>
                 <div className="col">
-                  <h2>Hello World, <span className="text-primary font-italic">Charles</span>.</h2>
-                  <input type="text" placeholder="Enter a name..." className="form-control"></input>
+                  <h2>Hello World, <span className="text-primary font-italic">{this.state.name}</span>.</h2>
+                  <input
+                    type="text"
+                    placeholder="Enter a name..."
+                    className="form-control"
+                    onChange={e => this.handleName(e)}>
+                  </input>
                 </div>
                 <div className="col"></div>
               </div>
