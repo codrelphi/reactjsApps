6943e0646fedcbb7c42b6ed2e0beb71be3b4f57b
codrelphi <chancerel.codjovi@gmail.com>
2019-11-16 20:39:16 +0100

    create the App component and render a bootstrap UI

--- hello-world-bs/src/App.js
+++ hello-world-bs/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (<div className="container">
+              <div className="row">
+                <div className="col"></div>
+                <div className="col">
+                  <h2>Hello World, <span className="text-primary font-italic">Charles</span>.</h2>
+                  <input type="text" placeholder="Enter a name..." className="form-control"></input>
+                </div>
+                <div className="col"></div>
+              </div>
+            </div>)
+  }
+
 }
 
 export default App;
