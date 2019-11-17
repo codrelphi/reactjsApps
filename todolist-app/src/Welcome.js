9cbd2a5ed6eaf99205afaa2a2cbf134dcdb3ef38
codrelphi <chancerel.codjovi@gmail.com>
2019-11-17 06:01:34 +0100

    write code to add 1 and remove 1

--- todolist-app/src/Welcome.js
+++ todolist-app/src/Welcome.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 
 class Welcome extends Component {
 
+  constructor() {
+    super(); // call the constructor of the parent Component
+    this.state = {
+      count: 0
+    };
+    this.removeOne = this.removeOne.bind(this);
+  }
+
+  addOne() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  removeOne() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
   render() {
     return (
-      <h1>Welcome {this.props.name}</h1>
+      <div>
+        <h1>Welcome {this.props.name}</h1>
+        <p>Mon compteur: {this.state.count}</p>
+        <button onClick={() => this.addOne()}>Ajouter 1</button>
+        <button onClick={this.removeOne}>Enlever 1</button>
+      </div>
     );
   }
 }
