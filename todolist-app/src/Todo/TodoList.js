25e0c4dc238cd5ad3fe2ea901847ac39d02c08a7
codrelphi <chancerel.codjovi@gmail.com>
2019-11-17 14:32:01 +0100

    write methods "addTodo" and "deleteTodo"

--- todolist-app/src/Todo/TodoList.js
+++ todolist-app/src/Todo/TodoList.js
@@ -17,17 +17,45 @@ class TodoList extends Component {
     });
   }
 
+  addTodo(event) {
+    event.preventDefault();
+    this.setState({
+      userInput: '',
+      items: [...this.state.items, this.state.userInput]
+    });
+  }
+
+  deleteTodo(event) {
+    event.preventDefault(); // don't reload the page
+    const array = this.state.items;
+    const index = array.indexOf(String(event.target.value));
+    console.log(index)
+    array.splice(index, 1);
+    this.setState({items: array});
+  }
+
+  renderTodos() {
+    return this.state.items.map((item) => {
+        return (<div key={item}>
+                  {item} | <button onClick={this.deleteTodo.bind(this)}>X</button>
+                </div>);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Ma Todo list</h1>
-        <input
-          type="text"
-          placeholder="Renseigner un item"
-          value={this.state.userInput}
-          onChange={this.onChange.bind(this)}
-          />
-        <button>Ajouter</button>
+        <form>
+          <input
+            type="text"
+            placeholder="Renseigner un item"
+            value={this.state.userInput}
+            onChange={this.onChange.bind(this)}
+            />
+          <button onClick={this.addTodo.bind(this)}>Ajouter</button>
+        </form>
+        <div>{this.renderTodos()}</div>
       </div>
     );
   }
