7eda7b3ec7b779538ccdb19ed9c33d250e0d1fbf
codrelphi <chancerel.codjovi@gmail.com>
2019-11-17 06:32:09 +0100

    pass value to Welcome component

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -7,11 +7,7 @@ import './App.css';
 class App extends Component {
 
   render() {
-<<<<<<< HEAD
     return <Welcome name="Jackson" />;
-=======
-    return <Welcome />;
->>>>>>> 28d6d4f41ce3bc3d61889e7be95348782e061122
   }
 }
 
