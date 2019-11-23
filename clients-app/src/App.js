bb75fb87d9c3145bce253f36bc9b333abaaa649f
codrelphi <chancerel.codjovi@gmail.com>
2019-11-23 23:50:22 +0100

    display the clients component

--- clients-app/src/App.js
+++ clients-app/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    clients: [
+      {id: 1, nom: "Lior Chamla"},
+      {id: 2, nom: "Magali Pernin"},
+      {id: 3, nom: "Joseph Durand"}
+    ],
+    compteur: 0
+  }
+
+  handleClick = () => {
+    this.setState({
+      compteur: this.state.compteur + 1
+  });
+  }
+
+  render() {
+    const title = "Liste de clients";
+    return (
+      <div>
+        <h1>{title}</h1>
+        {this.state.compteur}
+        <button onClick={this.handleClick}>click me</button>
+        <ul>
+          {this.state.clients.map(client =>
+            <li>{client.nom} <button>X</button></li>
+          )}
+        </ul>
+        <form>
+          <input type="text" placeholder="Ajouter un client" />
+          <button>Confirmer</button>
+        </form>
+      </div>
+    );
+
+  }
 }
 
 export default App;
