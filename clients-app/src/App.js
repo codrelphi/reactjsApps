4667d3487124dcf7b75af091b19e1a72cc7b4253
codrelphi <chancerel.codjovi@gmail.com>
2019-11-24 12:17:45 +0100

    write the methods to add client and to delete a client

--- clients-app/src/App.js
+++ clients-app/src/App.js
@@ -1,44 +1,65 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
 
   state = {
     clients: [
-      {id: 1, nom: "Lior Chamla"},
-      {id: 2, nom: "Magali Pernin"},
-      {id: 3, nom: "Joseph Durand"}
+      {id: 1, nom: "John Doe"},
+      {id: 2, nom: "Jane Germain"},
+      {id: 3, nom: "Raoul Sambo"},
     ],
+    clientInput: ""
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const id = new Date().getTime();
+    const nom = this.state.clientInput;
+    const clients = this.state.clients.slice();
+    const client = {id: id, nom: nom};
+    clients.push(client);
+    this.setState({
+      clients: clients,
+      clientInput: ""
+    });
+  }
+
+  handleChange = (event) => {
+    const clientInput = event.currentTarget.value;
+    this.setState({clientInput: clientInput});
   }
 
   handleDelete = (id) => {
-    const clients = this.state.clients.slice()
-    const index = clients.findIndex(client => client.id === id);
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(client =>
+      client.id === id);
     clients.splice(index, 1);
     this.setState({
       clients: clients
     });
-  };
+  }
 
   render() {
     const title = "Liste de clients";
     return (
       <div>
         <h1>{title}</h1>
-        <ul>
-          {this.state.clients.map(client =>
-            <li>{client.nom} <button onClick={() => this.handleDelete(client.id)}>X</button></li>
-          )}
-        </ul>
-        <form>
-          <input type="text" placeholder="Ajouter un client" />
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            { this.state.clients.map(client =>
+                <li key={client.id}>{client.nom} <button onClick={() => this.handleDelete(client.id)}>X</button></li>) }
+          </ul>
+          <input
+            type="text"
+            placeholder="Ajouter un nouveau client"
+            value={this.state.clientInput}
+            onChange={this.handleChange}
+          />
           <button>Confirmer</button>
         </form>
       </div>
     );
-
   }
 }
 
