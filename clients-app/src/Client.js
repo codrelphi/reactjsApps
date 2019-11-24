643849d484c799b6e92e28b7d7bbb09388e455ff
codrelphi <chancerel.codjovi@gmail.com>
2019-11-24 23:10:04 +0100

    update: composant fonctionnel simple

--- clients-app/src/Client.js
+++ clients-app/src/Client.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react';
 
 
-class Client extends Component {
-
-  render() {
-    const {client, handleDelete} = this.props;
+function Client(props) {
+    const {client, handleDelete} = props;
     return (
         <li key={client.id}>{client.nom}
           <button onClick={() => handleDelete(client.id)}>X</button>
         </li>
       );
-  }
 }
 
 export default Client;
