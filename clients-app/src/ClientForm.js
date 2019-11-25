a7b7d3048328468fb22e4bac3d0b045ea933454f
codrelphi <chancerel.codjovi@gmail.com>
2019-11-25 01:52:02 +0100

    update: add materialisecss tags

--- clients-app/src/ClientForm.js
+++ clients-app/src/ClientForm.js
@@ -17,8 +17,11 @@ class ClientForm extends Component {
     const id = new Date().getTime();
     const nom = this.state.clientInput;
 
-    this.props.handleAddClient({id, nom});
-    this.setState({clientInput: ""});
+    if (nom.length > 0) {
+      this.props.handleAddClient({id, nom});
+      this.setState({clientInput: ""});
+    }
+
   }
 
   render() {
@@ -27,10 +30,14 @@ class ClientForm extends Component {
         <input
           type="text"
           placeholder="Ajouter un nouveau client"
+          maxlength="52"
           value={this.state.clientInput}
           onChange={this.handleChange}
         />
-        <button>Confirmer</button>
+        <button className="btn waves-effect waves-light red lighten-1">
+          Confirmer
+          <i className="material-icons right">send</i>
+        </button>
       </form>
     );
   }
