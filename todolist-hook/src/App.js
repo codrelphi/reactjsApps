447e8ae52afffd2f97613c0be7c909f805b7e60d
codrelphi <chancerel.codjovi@gmail.com>
2019-11-29 22:26:44 +0100

    write the working code using class component

--- todolist-hook/src/App.js
+++ todolist-hook/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    tasks: [
+      {id: 1, name: "Task 1"},
+      {id: 2, name: "Task 2"},
+    ],
+    userInput: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      userInput: e.currentTarget.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.userInput.trim().length > 0) {
+      const tasks = [...this.state.tasks];
+      const id = new Date().getTime();
+      const name = this.state.userInput;
+      const task = {id: id, name: name}
+      tasks.push(task);
+      this.setState({tasks: tasks});
+      this.setState({userInput: ""});
+    }
+  }
+
+  handleDelete = (id) => {
+    const tasks = [...this.state.tasks];
+    const index = tasks.findIndex(task => task.id === id);
+    tasks.splice(index, 1);
+    this.setState({
+      tasks: tasks
+    });
+
+  }
+
+
+  render(){
+    return (<div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="add a task"
+            value={this.state.userInput}
+            onChange={this.handleChange}
+            />
+          <button>Add</button>
+        </form>
+        <ul>
+          {
+            this.state.tasks.map(task => (
+              <li key={task.id}>
+                {task.name}
+                <button onClick={() => this.handleDelete(task.id)}>X</button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>);
+  }
 }
 
 export default App;
